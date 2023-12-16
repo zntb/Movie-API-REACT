7ac6592e8334c39388eb7f34039603a723dcfbbe
zntb <zenetibi@yahoo.com>
2023-12-16 09:38:11 +0200

    Add AddMovie.js and styles for this component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -13,7 +14,6 @@ function App() {
     setError(null);
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -24,8 +24,8 @@ function App() {
         return {
           id: movieData.episode_id,
           title: movieData.title,
-          releaseDate: movieData.release_date,
           openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
         };
       });
       setMovies(transformedMovies);
@@ -38,6 +38,10 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -55,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
